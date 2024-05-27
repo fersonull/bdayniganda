13a21094db3daa632866807226f41aaa1ce36cfd
fersonull <158756047+fersonull@users.noreply.github.com>
2024-05-27 12:25:21 +0800

    Update bday2.js

--- bday2.js
+++ bday2.js
@@ -1,5 +1,18 @@
 const cake = document.querySelector('.container')
+const letter = document.querySelector('.letter-card')
+const text = document.querySelector('.scrttext')
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const music = new Audio("About You.mp3")
+    music.play()
+})
 
 cake.addEventListener('click',()=>{
     cake.classList.add('close')
+    letter.classList.add('show-letter')
+})
+
+letter.addEventListener('click',()=>{
+    cake.classList.remove('close')
+    letter.classList.remove('show-letter')
 })
